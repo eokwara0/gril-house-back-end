e6878a8632a0c68085ac373a6e67a469a8d05318
eokwara <emmanuelokwara001@gmail.com>
2023-03-22 10:25:06 +0200

    fix: :sparkles: added some basic functionality for user object

--- src/model/users/users.controller.ts
+++ src/model/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { User } from './user.models/users.shema';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor( private userService : UsersService ){}
+
+    @Get()
+    findAll(): Promise<User[]>{
+        return this.userService.findAll();
+    }
+
+    @Post()
+    @UsePipes( new ValidationPipe({ 
+        transform : true , 
+        skipMissingProperties : true , 
+        skipNullProperties : true , 
+        skipUndefinedProperties : true , 
+    }))
+    addUser(@Body() user: User): Promise<User>{
+        return this.userService.createUser( user );
+    }
+}
--- src/model/users/users.service.ts
+++ src/model/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.models/users.shema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectModel(User.name) private userModel : Model<UserDocument>){}
+
+    // Adding a new user to the database
+    async createUser( user : User) : Promise<User>{
+        const user1 = new this.userModel( user );
+        return user1.save();
+
+    }
+
+    /**
+     * TODO :: add support for udpating a user 
+     */ 
+    async updateUser( user : User )  {
+        this.userModel.updateOne( { })
+    }
+
+    // getting all the users in the database
+    async findAll() : Promise<User[]>{
+        return this.userModel.find().exec();
+    }
+}
